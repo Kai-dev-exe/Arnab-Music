be99b85acd9a6e64f2a0183c2913bf504b87da2f
ArnabBiswas18 <arunbiswas260@gmail.com>
2025-01-23 14:18:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://purnimabiswas2345:23452345@cluster0.1ewmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongodbUri : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
