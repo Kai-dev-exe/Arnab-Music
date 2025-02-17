97c8fca6ef0b633a7788793365dde7ca73f43c58
Kai-dev-exe <purnimabiswas2345@gmail.com>
2025-02-17 14:08:51 +0530

    config.js

--- config.js
+++ config.js
@@ -20,10 +20,10 @@ module.exports = {
   "secure": false
 },
 {
-  "identifier": "ChalresNaig Node",
-  "password": "NAIGLAVA-dash.techbyte.host",
-  "host": "lavahatry4.techbyte.host",
-  "port": 3000,
+  "identifier": "Public Lavalink v4 NonSSL",
+  "password": "https://dsc.gg/ajidevserver",
+  "host": "lava-v4.ajieblogs.eu.org",
+  "port": 80,
   "secure": false
 }
   ]
