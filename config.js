748b1343038a92cc92c5c5d7b2110de69645c99e
Kai-dev-exe <purnimabiswas2345@gmail.com>
2025-02-17 14:02:12 +0530

    config.js

--- config.js
+++ config.js
@@ -13,11 +13,18 @@ module.exports = {
   errorLog: "", 
   nodes: [
     {
-      name: "Koi Node V4",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
-    }
+  "identifier": "INZEWORLD.COM (DE)",
+  "password": "saher.inzeworld.com",
+  "host": "lava.inzeworld.com",
+  "port": 3128,
+  "secure": false
+},
+{
+  "identifier": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
+}
   ]
 }
