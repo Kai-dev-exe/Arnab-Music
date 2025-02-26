38e5c9b1577eb7a19579a65061b86250a1dd2a40
Kai-dev-exe <purnimabiswas2345@gmail.com>
2025-02-26 22:01:25 +0530

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 
-
 module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://purnimabiswas2345:23452345@cluster0.1ewmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -14,7 +13,7 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-    
+
     {
       name: "ARNAB LAVALINK",
       password: "arnab2345",
