41de96056266fa155eca7dabffb87f8dcb7bb3b4
ArnabBiswas18 <biswasarnab2005@gmail.com>
2025-03-06 15:00:13 +0530

    Spotify added

--- config.js
+++ config.js
@@ -5,23 +5,23 @@ module.exports = {
   language: "en",
   ownerID: ["1004206704994566164", ""], 
   mongodbUri : "",
+  spotifyClientId : "",
+  spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/qX9ugahuqh",
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-    
-    {
-      name: "ARNAB LAVALINK",
-      password: "arnab2345",
+     {
+      name: "XX",
+      password: "enteryourcustompass",
       host: "180.188.226.76",
-      port:  5405,
+      port:  7019,
       secure: false
     }
-
   ]
 }
