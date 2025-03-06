27733a89c673e53e1590c898cda744ff8731429d
Kai-dev-exe <purnimabiswas2345@gmail.com>
2025-03-06 15:10:41 +0530

    config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://purnimabiswas2345:23452345@cluster0.1ewmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  spotifyClientId : "b4a81d8295cd481e8d539a409ce9accb",
+  spotifyClientSecret : "8f3127d2007c4702acdf8e64143dd2e4",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
+      name: "ARNAB LAVALINK",
+      password: "arnab2345",
       host: "180.188.226.76",
-      port:  7019,
+      port:  5405,
       secure: false
     }
   ]
