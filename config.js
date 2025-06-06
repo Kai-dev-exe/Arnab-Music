5ca597d42c4b204eed402d35b3bc5acffa21b9c6
Kai-dev-exe <purnimabiswas2345@gmail.com>
2025-06-06 13:04:17 +0530

    config.js

--- config.js
+++ config.js
@@ -1,27 +1,25 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://purnimabiswas2345:23452345@cluster0.1ewmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  spotifyClientId : "b4a81d8295cd481e8d539a409ce9accb",
+  spotifyClientSecret : "8f3127d2007c4702acdf8e64143dd2e4",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/qX9ugahuqh",
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "",
-      password: "",
-      host: "",
-      port:  ,
-      secure: 
+      name: "Arnab Lavalink",
+      password: "arnab2345",
+      host: "meta.pylex.xyz",
+      port:  11562,
+      secure: false
     }
   ]
 }
